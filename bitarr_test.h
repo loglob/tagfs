2cd0485a992e4c0f59ad36e5aaae0f110b514c1b
loglob <23386237+loglob@users.noreply.github.com>
2020-10-08 02:41:12 +0200

    Added test for bitarr_resize

--- bitarr_test.h
+++ bitarr_test.h
@@ -1,6 +1,8 @@
 #include "bitarr.h"
 #define PARAM_T bitarr_t
 #include "test.h"
+#include <string.h>
+#include <errno.h>
 
 #define BASE_LEN 64
 
@@ -53,6 +55,23 @@ int testnext3()
 	#undef next
 }
 
+int testResize()
+{
+	bitarr_t arr = bitarr_new(BASE_LEN);
+	assertMsg(bitarr_count(arr, BASE_LEN, true) == 0, "newly allocated bitarray contains 1s\n");
+	bitarr_fill(arr, 0, BASE_LEN, true);
+
+	arr = bitarr_resize(arr, BASE_LEN, BASE_LEN / 2);
+	assertMsg(arr, "bitarr_resize() shrinking failed: %s\n", strerror(errno));
+	assertMsg(bitarr_count(arr, BASE_LEN / 2, false) == 0, "resize changed value\n")
+
+	arr = bitarr_resize(arr, BASE_LEN / 2, BASE_LEN);
+	assertMsg(arr, "bitarr_resize() growing failed: %s\n", strerror(errno));
+	assertMsg(bitarr_count(arr, BASE_LEN, true) == BASE_LEN / 2, "resized portion of bitarray contains 1s\n");
+
+	return EXIT_SUCCESS;
+}
+
 int testSimple(bitarr_t arr)
 {
 	assertMsg(bitarr_get(arr, 5) == 0, "init failure!")
@@ -75,6 +94,6 @@ bitarr_t newBitarr()
 	return bitarr_new(BASE_LEN);
 }
 
-const test_t tests[] = { testnext1, testnext2, testnext3 };
+const test_t tests[] = { testnext1, testnext2, testnext3, testResize };
 const ptest_t ptests[] = { testSimple };
 const factory_t factories[] = { (factory_t){ bitarr_destroy, newBitarr } };
